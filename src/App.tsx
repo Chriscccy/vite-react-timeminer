a93ddc0ff38988b4722ef3d64d8d35fb49a0e153
RichTechOFS <richtechlabs@gmail.com>
2025-04-07 16:11:57 +0800

    上线

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React hello world 我在测试</h1>
+      <h1>Vite + React hello world 我在测试 快速上线</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
